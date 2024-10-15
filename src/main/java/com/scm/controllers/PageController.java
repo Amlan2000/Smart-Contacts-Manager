b18c3bfa0d326443c3142985b094e2517e6b3395
amlan2000 <amlanmishra0911@gmail.com>
2024-10-15 23:03:25 +0530

    basic SCM working

---
+++ src/main/java/com/scm/controllers/PageController.java
@@ -0,0 +1,95 @@
+package com.scm.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.scm.entities.User;
+import com.scm.forms.UserForm;
+import com.scm.helpers.Message;
+import com.scm.helpers.MessageType;
+import com.scm.services.UserService;
+
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@Controller
+public class PageController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/")
+    public String index() {
+        return "redirect:/home";
+    }
+
+    @RequestMapping("/home")
+    public String home(Model model) {
+        model.addAttribute("name", "MyTech");
+        model.addAttribute("githubRepo", "https://github.com/Amlan2000/");
+        return "home";
+    }
+
+
+    // contact page
+    @GetMapping("/contact")
+    public String contact() {
+        return new String("contact");
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return new String("login");
+    }
+
+    // registration page
+    @GetMapping("/register")
+    public String register(Model model) {
+
+        UserForm userForm = new UserForm();
+        model.addAttribute("userForm", userForm);
+
+        return "register";
+    }
+
+    // processing register
+
+    @RequestMapping(value = "/do-register", method = RequestMethod.POST)
+    public String processRegister(@Valid @ModelAttribute UserForm userForm, BindingResult rBindingResult,
+            HttpSession session) {
+
+
+        // validate form data
+        if (rBindingResult.hasErrors()) {
+            return "register";
+        }
+
+
+        User user = new User();
+        user.setName(userForm.getName());
+        user.setEmail(userForm.getEmail());
+        user.setPassword(userForm.getPassword());
+        user.setAbout(userForm.getAbout());
+        user.setPhoneNumber(userForm.getPhoneNumber());
+        user.setEnabled(false);
+        user.setProfilePic(
+                "https://static-00.iconduck.com/assets.00/profile-default-icon-2048x2045-u3j7s5nj.png");
+
+        User savedUser = userService.saveUser(user);
+
+
+        Message message = Message.builder().content("Registration Successful").type(MessageType.green).build();
+
+        session.setAttribute("message", message);
+
+        return "redirect:/register";
+    }
+
+}
---
+++ src/main/java/com/scm/services/ContactService.java
@@ -0,0 +1,33 @@
+package com.scm.services;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.scm.entities.Contact;
+import com.scm.entities.User;
+
+public interface ContactService {
+    Contact save(Contact contact);
+
+    Contact update(Contact contact);
+
+    List<Contact> getAll();
+
+    Contact getById(String id);
+
+    void delete(String id);
+
+    // search contact
+    Page<Contact> searchByName(String nameKeyword, int size, int page, String sortBy, String order, User user);
+
+    Page<Contact> searchByEmail(String emailKeyword, int size, int page, String sortBy, String order, User user);
+
+    Page<Contact> searchByPhoneNumber(String phoneNumberKeyword, int size, int page, String sortBy, String order,
+            User user);
+
+
+    Page<Contact> getByUser(User user, int page, int size, String sortField, String sortDirection);
+
+}
